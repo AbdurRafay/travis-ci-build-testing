6847ca53d426143689512e81ad31018c4c9a9751
ABDUR RAFAY <arafay@folio3.com>
2018-07-09 17:54:38 +0500

    travis request

--- ios-pre-build-script.sh
+++ ios-pre-build-script.sh
@@ -1,11 +1,25 @@
-SDK_BRANCH=${1:-'master'}							#arafay/BotFiltering
-DIRECTORY=$(cd `dirname $0` && pwd)
+# SDK_BRANCH=${1:-'master'}							#arafay/BotFiltering
+# DIRECTORY=$(cd `dirname $0` && pwd)
 
-# echo "${DIRECTORY}"
-# cd $DIRECTORY
+# # echo "${DIRECTORY}"
+# # cd $DIRECTORY
 
-# git reset --hard
+# # git reset --hard
 
-sed -i '' "s~\'master\'~'${SDK_BRANCH}'~g" 'Podfile'
+# sed -i '' "s~\'master\'~'${SDK_BRANCH}'~g" 'Podfile'
 
-pod install
+# pod install
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token UWcEAckPxu9vRUFy7Ma0UQ" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/AbdurRafay%2Ftravis-ci-build-testing/requests
+  
